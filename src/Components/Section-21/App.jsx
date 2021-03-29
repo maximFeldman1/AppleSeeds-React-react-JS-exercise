110048f989608a56afd964934d1ce40fec8ef7c7
Maxim Feldman <43370411+maximFeldman1@users.noreply.github.com>
2021-03-29 11:57:24 +0300

    Create App.jsx

---
+++ src/Components/Section-21/App.jsx
@@ -0,0 +1,16 @@
+import Text from "./React-Hooks/18.1/Text";
+import react from "react";
+
+const items = {
+  text:
+    "Lorem ipsum dolor,Lorem ipsum ,Lorem ipsum dolor sit amet, consectetur adipisicing..Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum quas veniam hic velit enim  elit. Nostrum quas veniam hic velit enimdolor sit amet, consectetur adipisicing elit. Nostrum quas veniam hic velit enim sit amet, consectetur adipisicing elit. Nostrum quas veniam hic velit enim odit nemo dolorum ad exercitationem animi assumend   officiis eius, et provident ea quisquam quibusdam, culpa voluptates!",
+  length: 150,
+};
+
+export default () => {
+  return (
+    <div>
+      <Text items={items} />
+    </div>
+  );
+};
